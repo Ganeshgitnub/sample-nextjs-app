27bf725dd0f03948635512f9e692f470e03653f4
Ganesh <122281957+Ganeshgitnub@users.noreply.github.com>
2023-10-27 10:07:33 +0530

    Update before_install.sh

--- aws/before_install.sh
+++ aws/before_install.sh
@@ -7,18 +7,18 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 source ~/.bashrc
 
 #create our working directory if it doesnt exist
-DIR="/opt/frontend"
+DIR="/root/frontend"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
-  rm -rf /opt/frontend/
+  rm -rf /root/frontend/
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
  
 #delete all the files in current directory
-rm -rf /opt/frontend/*
-rm -rf /opt/frontend/.gitignore
+rm -rf /root/frontend/*
+rm -rf /root/frontend/.gitignore
 
 nvm install node
 npm -v

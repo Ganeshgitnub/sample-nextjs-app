de6afbd34b5126848d689ed30e7fa9c28eabdffd
Ganesh <122281957+Ganeshgitnub@users.noreply.github.com>
2023-10-27 10:26:48 +0530

    Update after_install.sh

--- aws/after_install.sh
+++ aws/after_install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd /opt/frontend/
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+ 
+# remove module files
+rm -rf node_modules package-lock.json
+pwd
 npm install
 npm run build
 npm install pm2 -g

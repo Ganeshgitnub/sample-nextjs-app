30d0a5373cf18925e5a3c6166fa03d16cd729dfa
Ganesh <122281957+Ganeshgitnub@users.noreply.github.com>
2023-10-30 12:23:42 +0530

    Update after_install.sh

--- aws/after_install.sh
+++ aws/after_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /opt/frontend/
-#add npm and node to path
+
+#add path
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
